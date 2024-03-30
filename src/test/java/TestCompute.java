f644fa83fc1dfa7afe95ba686297575661e14d68
taksu06 <tugberkaksu@etu.edu.tr>
2024-03-31 01:35:41 +0300

    code coverage of the compute.java up to %100

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -21,7 +21,7 @@ public class TestCompute {
         MessageQueue mq = mock(MessageQueue.class);
         when(mq.size()).thenReturn(0);
         c = new Compute(mq);
-        assertEquals(-1, c.countNumberOfOccurrences(""));
+      assertEquals("Count of occurrences when queue size is zero", -1, c.countNumberOfOccurrences(""));
     }
 
     @Test
@@ -30,8 +30,29 @@ public class TestCompute {
         when(mq.size()).thenReturn(1);
         when(mq.contains("")).thenReturn(false);
         c = new Compute(mq);
-        assertEquals(0, c.countNumberOfOccurrences(""));
+        assertEquals("Count of occurrences when element does not exist in queue", 0, c.countNumberOfOccurrences(""));
     }
 
+    @Test
+    public void testCountNumberOfOccurrencesSingleOccurrence() {
+        MessageQueue mq = mock(MessageQueue.class);
+        when(mq.size()).thenReturn(1);
+        when(mq.contains("")).thenReturn(true);
+        when(mq.getAt(0)).thenReturn("");
+        c = new Compute(mq);
+        assertEquals("Count of occurrences for a single occurrence", 1, c.countNumberOfOccurrences(""));
+    }
+
+    @Test
+    public void testCountNumberOfOccurrencesMultipleOccurrences() {
+        MessageQueue mq = mock(MessageQueue.class);
+        when(mq.size()).thenReturn(3);
+        when(mq.contains("")).thenReturn(true);
+        when(mq.getAt(0)).thenReturn("");
+        when(mq.getAt(1)).thenReturn("a");
+        when(mq.getAt(2)).thenReturn("");
+        c = new Compute(mq);
+        assertEquals("Count of occurrences for multiple occurrences", 2, c.countNumberOfOccurrences(""));
+    }
 
 }
